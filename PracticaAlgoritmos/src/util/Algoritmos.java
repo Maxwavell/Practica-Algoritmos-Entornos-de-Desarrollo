a989720a5eb6600f8553bad3e7460f23d43dc414
Maxwavell <145749139+Maxwavell@users.noreply.github.com>
2024-04-21 20:32:28 +0200

    fibonacci

---
+++ PracticaAlgoritmos/src/util/Algoritmos.java
@@ -0,0 +1,12 @@
+package util;
+
+public abstract class Algoritmos {
+
+	public static int fibonacci(int numero) {
+	        if (numero <= 1) {
+	            return numero;
+	        } else {
+	            return fibonacci(numero - 1) + fibonacci(numero - 2);
+	        }
+	    }
+}
